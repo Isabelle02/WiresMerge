5ad2d5fcf0c8bc2b305da39494daa8fc067b7a7a
SofiKaeyd <sfbglv@gmail.com>
2025-11-18 17:03:48 +0300

    impl popups Question, Lose, Win

--- Assets/Scripts/Window/GameWindow.cs
+++ Assets/Scripts/Window/GameWindow.cs
@@ -5,10 +5,22 @@ public class GameWindow : BaseWindow
 {
     [SerializeField] private BaseButton _pauseButton;
 
+    [SerializeField] private BaseButton _Button;
+    [SerializeField] private BaseButton _ButtonLose;
+    [SerializeField] private BaseButton _ButtonWin;
+
     public override async UniTask OnOpen()
     {
         _pauseButton.OnButtonClick += OnPauseClick;
         MouseManager.AddClickable(_pauseButton);
+
+
+        _Button.OnButtonClick += OndClick;
+        _ButtonLose.OnButtonClick += OnLoseClick;
+        _ButtonWin.OnButtonClick += OnWinClick;
+        MouseManager.AddClickable(_Button);
+        MouseManager.AddClickable(_ButtonLose);
+        MouseManager.AddClickable(_ButtonWin);
     }
 
     private void OnPauseClick(BaseButton button)
@@ -16,9 +28,32 @@ public class GameWindow : BaseWindow
         WindowManager.Open<PausePopup>();
     }
 
+    private void OndClick(BaseButton button)
+    {
+        WindowManager.Open<QuestionPopup>();
+    }
+
+    private void OnLoseClick(BaseButton button)
+    {
+        WindowManager.Open<LosePopup>();
+    }
+
+    private void OnWinClick(BaseButton button)
+    {
+        WindowManager.Open<WinPopup>();
+    }
+
     public override async UniTask OnClose()
     {
         _pauseButton.OnButtonClick -= OnPauseClick;
         MouseManager.RemoveClickable(_pauseButton);
+
+
+        _Button.OnButtonClick -= OndClick;
+        _ButtonLose.OnButtonClick -= OnLoseClick;
+        _ButtonWin.OnButtonClick -= OnWinClick;
+        MouseManager.RemoveClickable(_Button);
+        MouseManager.RemoveClickable(_ButtonLose);
+        MouseManager.RemoveClickable(_ButtonWin);
     }
 }
