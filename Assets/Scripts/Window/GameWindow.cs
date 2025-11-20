f5c2c90eb357b73cf3edc7f347fdcca6b463d6cf
SofiKaeyd <sfbglv@gmail.com>
2025-11-20 16:24:27 +0300

    Revert "impl popups Question, Lose, Win"
    This reverts commit 5ad2d5fcf0c8bc2b305da39494daa8fc067b7a7a.

--- Assets/Scripts/Window/GameWindow.cs
+++ Assets/Scripts/Window/GameWindow.cs
@@ -5,22 +5,10 @@ public class GameWindow : BaseWindow
 {
     [SerializeField] private BaseButton _pauseButton;
 
-    [SerializeField] private BaseButton _Button;
-    [SerializeField] private BaseButton _ButtonLose;
-    [SerializeField] private BaseButton _ButtonWin;
-
     public override async UniTask OnOpen()
     {
         _pauseButton.OnButtonClick += OnPauseClick;
         MouseManager.AddClickable(_pauseButton);
-
-
-        _Button.OnButtonClick += OndClick;
-        _ButtonLose.OnButtonClick += OnLoseClick;
-        _ButtonWin.OnButtonClick += OnWinClick;
-        MouseManager.AddClickable(_Button);
-        MouseManager.AddClickable(_ButtonLose);
-        MouseManager.AddClickable(_ButtonWin);
     }
 
     private void OnPauseClick(BaseButton button)
@@ -28,32 +16,9 @@ public class GameWindow : BaseWindow
         WindowManager.Open<PausePopup>();
     }
 
-    private void OndClick(BaseButton button)
-    {
-        WindowManager.Open<QuestionPopup>();
-    }
-
-    private void OnLoseClick(BaseButton button)
-    {
-        WindowManager.Open<LosePopup>();
-    }
-
-    private void OnWinClick(BaseButton button)
-    {
-        WindowManager.Open<WinPopup>();
-    }
-
     public override async UniTask OnClose()
     {
         _pauseButton.OnButtonClick -= OnPauseClick;
         MouseManager.RemoveClickable(_pauseButton);
-
-
-        _Button.OnButtonClick -= OndClick;
-        _ButtonLose.OnButtonClick -= OnLoseClick;
-        _ButtonWin.OnButtonClick -= OnWinClick;
-        MouseManager.RemoveClickable(_Button);
-        MouseManager.RemoveClickable(_ButtonLose);
-        MouseManager.RemoveClickable(_ButtonWin);
     }
 }
